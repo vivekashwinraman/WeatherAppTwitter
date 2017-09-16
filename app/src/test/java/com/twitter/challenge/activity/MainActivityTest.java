651a772b637f24efd13325b232e8ec5e391807e5
“Radhika <radhika.sampath@capitalone.com>
2017-09-16 19:41:13 -0400

    there are errors

---
+++ app/src/test/java/com/twitter/challenge/activity/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.twitter.challenge.activity;
+
+
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+import android.support.test.runner.AndroidJUnit4;
+
+/**
+ * Created by edk763 on 9/16/17.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+}
