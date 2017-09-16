7f73c13454d86b73cdd41208c9ffd6925a0651e3
“Radhika <radhika.sampath@capitalone.com>
2017-09-16 18:38:09 -0400

    5 day basic

--- app/src/main/java/com/twitter/challenge/network/WeatherClient.java
+++ app/src/main/java/com/twitter/challenge/network/WeatherClient.java
@@ -2,7 +2,9 @@ package com.twitter.challenge.network;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.schedulers.Schedulers;
 
 
 /**
@@ -13,15 +15,15 @@ public class WeatherClient {
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
+        RxJavaCallAdapterFactory rxAdapter = RxJavaCallAdapterFactory.createWithScheduler(Schedulers.io());
 
         OkHttpClient client = new OkHttpClient.Builder().build();
         retrofit = new Retrofit.Builder()
                 .baseUrl("http://twitter-code-challenge.s3-website-us-east-1.amazonaws.com")
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(rxAdapter)
                 .client(client)
                 .build();
-
-
 
         return retrofit;
     }
