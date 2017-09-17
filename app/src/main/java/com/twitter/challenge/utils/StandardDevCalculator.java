0282392154f707f26eee383be857e905959e9857
“Radhika <radhika.sampath@capitalone.com>
2017-09-16 22:08:34 -0400

    std dev

---
+++ app/src/main/java/com/twitter/challenge/utils/StandardDevCalculator.java
@@ -0,0 +1,12 @@
+package com.twitter.challenge.utils;
+
+/**
+ * Created by edk763 on 9/16/17.
+ */
+
+public class StandardDevCalculator {
+    public static float calculate() {
+
+        return 1f;
+    }
+}
